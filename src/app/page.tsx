f5049ebdec360ea5162f8c76cf739cdadc96d3f6
Bluzzi <camilledugas.pro@gmail.com>
2023-11-10 10:33:29 +0100

    fix(dev.db): update dev.db binary file fix(page.tsx): remove unnecessary async/await keywords in queryFn to improve code readability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home(): ReactElement {
 
   const { status, data } = useQuery({
     queryKey: ["getUsers"],
-    queryFn: async() => await getUsers()
+    queryFn: () => getUsers()
   });
 
   const create = useMutation({
